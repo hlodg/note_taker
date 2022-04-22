ef59e46c562bafcb15e5dcc0dc8b9a555982db8b
Hannah Lodge <lodg0021@umn.edu>
2022-04-21 23:28:19 -0500

    working on functions

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 // Create a server.js file that will serve up the application
 //     - Import all your required dependencies
 //     - Initialize the Express app
+const { DESTRUCTION } = require('dns');
 const express = require('express');
 //     - Set up the port that your server will listen on (NOTE: Heroku will set this up for you when you deploy)
 const app = express();
 const path = require('path');
-const { getEnabledCategories } = require('trace_events');
+const middleware = require(middleware);
 const PORT = process.env.PORT || 3000;
 //     - Set body parsing, static middleware, route middleware
-// app.use(middleware);
+app.use(middleware);
 //     - Start the server
 
 // const apiRoutes = require('./routes/apiRoutes');
@@ -24,10 +25,10 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/note
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 //     - Create a GET route for `/api/notes` that returns all saved notes as JSON
-app.get('/api/notes', (req,res) => res.json('getNotes'));
+app.get('/api/notes', (req,res, getNotes()) => res.json('getNotes'));
 
 //     - Create a POST route for `api/notes` that saves a new note to the db.json file
-app.post ('/api/notes',(req,res) => res.json('setnotes'));
+app.post ('/api/notes',(req,res, saveNote()) => res.json('setnotes'));
 
 app.get('*',(req, res) => res.json('index.html'));
 
@@ -37,13 +38,17 @@ app.listen(PORT, () => console.log(`Listening on PORT: PORT`));
 
 // 4. Create helper functions that manage saving and retrieving notes from the db.json file
 //     -Create a getNotes() function that returns all the saved notes from the db.json file
-getNotes() => (fs.readFile('./db.json', utf8, (err, data)=> {
-    console.log(data)}));
+getNotes(data) { fs.readFile('./db.json', utf8, (err, data)=> {
+    console.log(data)})};
 
 //     -Create a saveNote() function that saves a new note to the db.json file and returns the new note as JSON
+const fileData = JSON.parse(fs.readFileSync('db.json'))
+fileData.push(newData)
 
+saveNote(newData) { 
+    fs.writeFileSync('/db.json', JSON.stringify(fileData, null, 2))
+};
 
-//     -Create a deleteNote() function that deletes a note from the db.json file and returns a success message (bonus)
 
 // 5. Integrate your helper functions into the routes
 
